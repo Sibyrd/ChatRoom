46f50925ad5bbd85d69d0f515a3ca0e9e3863689
Sibyrd <mail@theobird.com>
2024-11-10 12:49:29 +0000

    added discord integration converted to ES modules

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const websocket = require("ws");
-const server = new websocket.Server({ port: 1273 })
-
+import { WebSocketServer } from 'ws';
+const server = new WebSocketServer({ port: 1273 })
+const discordwebhook = "URLHERE"
+import fetch from "node-fetch";
 
 server.on("connection", socket => {
     let count = 0;
@@ -21,7 +22,35 @@ server.on("connection", socket => {
             console.log(currmessage);
             server.clients.forEach(client => {
                 client.send(currmessage);
-            })
+            });
+            var params = {
+                embeds: [
+                    {
+                        "title": `New Message`,
+                        "color": 9498256,
+                        "thumbnail": {
+                            "url": "",
+                        },
+                        "fields": [
+                            {
+                                "name": `${username}`,
+                                "value": `${message}`,
+                                "inline": true
+                            }
+                        ]
+                    }
+                ]
+            };
+            fetch(discordwebhook, {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(params)
+            }).then(res => {
+                console.log(res);
+            });
         }
     })
 })
+
